d1236aa9eab8d6fd88b41275844b92df642322bd
shot <100574153+shotrecommendation@users.noreply.github.com>
2023-03-21 21:12:44 +0100

    Add gameOfRPS() function, modify return values of playRound()

--- rps.js
+++ rps.js
@@ -20,9 +20,33 @@ function getWinningMove(selectedMove) {
 function playRound(playerSelection, computerSelection) {
   let gameResult
   if (playerSelection.toLowerCase() === computerSelection.toLowerCase()) {
-    return "It's a TIE!";
+    return "TIE";
   } else {
     gameResult = (playerSelection.toLowerCase() === getWinningMove(computerSelection)) ? "WIN" : "LOSE";
   }
-  return `You ${gameResult}! ${playerSelection} beats ${computerSelection}!`;
+  return gameResult;
+};
+
+
+function gameOfRPS() {
+  let playersMove, computersMove, roundResult
+  let playersScore = 0
+  let computersScore = 0
+
+  for (i = 0; i < 5; i++) {
+    playersMove = prompt("Enter your Rock, Paper or Scissors move:");
+    computersMove = getComputerChoice();
+    roundResult = playRound(playersMove, computersMove);
+
+    if (roundResult === "WIN") playersScore++;
+    if (roundResult === "LOSE") computersScore++;
+    console.log(roundResult);
+  };
+
+  console.log(`Player ${playersScore}:${computersScore} Computer`);
+  if (playersScore > computersScore) console.log("PLAYER wins!");
+  if (playersScore === computersScore) console.log("It's a TIE!");
+  if (playersScore < computersScore) console.log("COMPUTER wins!");
 }
+
+gameOfRPS()
